6739da4d7399bf4228eb3cf7fec26ecf3192926d
Evan12Scott <evan.scott@dc.rr.com>
2023-09-08 12:42:58 -0700

    Initialize HashMap Interface

--- KWHashMap.java
+++ KWHashMap.java
@@ -1,2 +1,12 @@
+// creates the interface for the HashMap
 public interface KWHashMap {
+    V get(Object key);
+
+    V put(K key, V value);
+
+    V remove(Object key);
+
+    int size();
+
+    boolean isEmpty();
 }
