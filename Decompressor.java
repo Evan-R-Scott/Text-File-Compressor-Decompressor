4959a345e84b14afbeead1441c88d06f754c7e33
Evan12Scott <evan.scott@dc.rr.com>
2023-09-08 18:53:25 -0700

    Finished Decompressor

--- Decompressor.java
+++ Decompressor.java
@@ -3,132 +3,104 @@ import java.io.*;
 import java.util.Scanner;
 import java.lang.String;
 
-public class Decompress {
+public class Decompressor {
     public static void main(String[] args) {
-        12
-        13 // initialize necessary variables to calculate time
-        14 double startTime, endTime, totalTime;
-        15 startTime = System.nanoTime();
-        16
-        17 // initialize file variables put in command line
-        18 String input = "";
-        19 input = args[0];
-        20 File myFile;
-        21 myFile = new File(args[0]);
-        22
-        23 // more miscellaneous initializations, including scanner, table size, and .log string
-        24 String log = ".log";
-        25 int table_size = 233;
-        26 boolean valid = false;
-        27 BufferedReader fileBuff;
-        28 Scanner scan = new Scanner(System.in);
-        29
-        30 // do while loop that executes until a valid file is provided by user, prompts user to enter in new filename if not valid.
-        31 do {
-            32 try {
-                33 fileBuff = new BufferedReader(new FileReader(myFile));
-                34 valid = true;
-                35 } catch(FileNotFoundException e) {
-                36 System.out.println("Invalid filename. Try Again.");
-                37 System.out.println("Enter the filename.\n");
-                38 String filer = scan.nextLine();
-                39 input = filer;
-                40 myFile = new File(filer);
-                41 }
-            42 } while(!valid);
-        43
-        44 // implement HashTable into program set at table size initialized earlier.
-        45 HashTableChain<Integer, String> table = new HashTableChain<>(table_size);
-        46
-        47 // loops through 128 characters putting them in the hash table
-        48 for(int i = 0; i < 128; i++){
-            49 table.put(i, String.valueOf((char) i));
-            50 }
-        51
-        52 // cuts off the .zzz of the filename inputted by user
-        53 int dotIndex = input.lastIndexOf(".");
-        54 String filename = input.substring(0, dotIndex);
-        55
-        56 try {
-            57 // initializes all stream and writer objects to read in the binary file and write out the decompressed code
-            58 FileInputStream file = new FileInputStream(myFile);
-            59 ObjectInputStream obj = new ObjectInputStream(file);
-            60 FileWriter writer = new FileWriter(filename);
-            61
-            62 // initialize variables for hash table additions in while loop below
-            63 String entry = "";
-            64 int chFirst;
-            65 String newQ = "";
-            66 boolean val = true;
-            67 int count = 127;
-            68
-            69 // gets the p starting value of file and writes to hash table
-            70 int p = obj.readInt();
-            71 writer.write(table.get(p));
-            72
-            73 // iterates through the file until no more bytes are available to be read
-            74 while(obj.available() > 0){
-                75
-                76 // reads each binary value
-                77 chFirst = obj.readInt();
-                78
-                79 // first character is in the dict get it and assign it to entry
-                80 if(chFirst < table.size()){
-                    81 entry = table.get(chFirst);
-                    82
-                    83 // else, entry is assigned to p and the first character of previous
-                    84 } else {
-                    85 entry = table.get(p) + table.get(p).charAt(0);
-                    86
-                    87 }
-                88
-                89 // write to the file whatever entry was assigned to
-                90 writer.write(entry);
-                91
-                92 // accumulate count variable and insert the new pair found into the table
-                93 count++;
-                94 table.put(count, table.get(p)+entry.charAt(0));
-                95
-                96 // reassign p to chFirts
-                97 p = chFirst;
-                98
-                99 }
-            100
-            101 // close out of streams and writers
-            102 obj.close();
-            103 file.close();
-            104 }
-        105
-        106 // all catch blocks for possible throws
-        107 catch(StreamCorruptedException e){
-            108 System.out.println("Decompression Completed");
-            109 }
-        110
-        111 catch(FileNotFoundException e) {
-            112 System.out.println("File Not Found.\n" + e.getMessage());
-            113 }
-        114
-        115 catch(IOException e) {
-            116 System.out.println("An error occurred while writing to the file.\n" + e.getMessage());
-            117 }
-        118
-        119 // create Decompression log file
-        120 File newFile = new File(filename + log);
-        121
-        122 // calculate time took
-        123 endTime = System.nanoTime();
-        124 totalTime = (endTime - startTime) / 1000000000.0;
-        125
-        126 // try block that writes to log file the results
-        127 try {
-            128 FileWriter newWriter = new FileWriter(input + log);
-            129 newWriter.write("Decompression of " + input);
-            130 newWriter.write("\nDecompression took " + totalTime + " seconds.");
-            131 newWriter.write("\nThe table was doubled " + table.getRehashes() + " times.");
-            132 newWriter.close();
-            133 } catch(IOException e) {
-            134 System.out.println("An error occurred while writing to the file: " + e.getMessage());
-            135 }
-        136
-        137 }
+        // initialize necessary variables to calculate time
+        double startTime, endTime, totalTime;
+        startTime = System.nanoTime();
+        // initialize file variables put in command line
+        String input = "";
+        input = args[0];
+        File myFile;
+        myFile = new File(args[0]);
+        // more miscellaneous initializations, including scanner, table size, and .log string
+        String log = ".log";
+        int table_size = 233;
+        boolean valid = false;
+        BufferedReader fileBuff;
+        Scanner scan = new Scanner(System.in);
+        // do while loop that executes until a valid file is provided by user, prompts user to enter in new filename if not valid.
+        do {
+            try {
+                fileBuff = new BufferedReader(new FileReader(myFile));
+                valid = true;
+            } catch (FileNotFoundException e) {
+                System.out.println("Invalid filename. Try Again.");
+                System.out.println("Enter the filename.\n");
+                String filer = scan.nextLine();
+                input = filer;
+                myFile = new File(filer);
+            }
+        } while (!valid);
+        // implement HashTable into program set at table size initialized earlier.
+        HashTableChain<Integer, String> table = new HashTableChain<>(table_size);
+        // loops through 128 characters putting them in the hash table
+        for (int i = 0; i < 128; i++) {
+            table.put(i, String.valueOf((char) i));
+        }
+
+        // cuts off the .zzz of the filename inputted by user
+        int dotIndex = input.lastIndexOf(".");
+        String filename = input.substring(0, dotIndex);
+        try {
+            // initializes all stream and writer objects to read in the binary file and write out the decompressed code
+            FileInputStream file = new FileInputStream(myFile);
+            ObjectInputStream obj = new ObjectInputStream(file);
+            FileWriter writer = new FileWriter(filename);
+            // initialize variables for hash table additions in while loop below
+            String entry = "";
+            int chFirst;
+            String newQ = "";
+            boolean val = true;
+            int count = 127;
+            // gets the p starting value of file and writes to hash table
+            int p = obj.readInt();
+            writer.write(table.get(p));
+            // iterates through the file until no more bytes are available to be read
+            while (obj.available() > 0) {
+                // reads each binary value
+                chFirst = obj.readInt();
+                // first character is in the dict get it and assign it to entry
+                if (chFirst < table.size()) {
+                    entry = table.get(chFirst);
+                    // else, entry is assigned to p and the first character of previous
+                } else {
+                    entry = table.get(p) + table.get(p).charAt(0);
+                }
+                // write to the file whatever entry was assigned to
+                writer.write(entry);
+                // accumulate count variable and insert the new pair found into the table
+                count++;
+                table.put(count, table.get(p) + entry.charAt(0));
+                // reassign p to chFirts
+                p = chFirst;
+            }
+            // close out of streams and writers
+            obj.close();
+            file.close();
+        }
+        // all catch blocks for possible throws
+        catch (StreamCorruptedException e) {
+            System.out.println("Decompression Completed");
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found.\n" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.\n" + e.getMessage());
+        }
+        // create Decompression log file
+        File newFile = new File(filename + log);
+        // calculate time took
+        endTime = System.nanoTime();
+        totalTime = (endTime - startTime) / 1000000000.0;
+        // try block that writes to log file the results
+        try {
+            FileWriter newWriter = new FileWriter(input + log);
+            newWriter.write("Decompression of " + input);
+            newWriter.write("\nDecompression took " + totalTime + " seconds.");
+            newWriter.write("\nThe table was doubled " + table.getRehashes() + " times.");
+            newWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file: " + e.getMessage());
+        }
+    }
 }
